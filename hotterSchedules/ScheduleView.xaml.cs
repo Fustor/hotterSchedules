2eec12ad97d72953624b6e2a0e21ed842701e6a2
Fustor <zfaust88@gmail.com>
2018-05-24 14:48:01 -0500

    starting to show schedule

---
+++ hotterSchedules/ScheduleView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hotterSchedules
+{
+    /// <summary>
+    /// Interaction logic for ScheduleView.xaml
+    /// </summary>
+    public partial class ScheduleView : Window
+    {
+        public ScheduleView()
+        {//TODO: Find how to show the schedule on a datagrid
+            InitializeComponent();
+        }
+    }
+}
