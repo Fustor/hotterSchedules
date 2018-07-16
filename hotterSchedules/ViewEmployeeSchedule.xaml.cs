28ef4faa5e3974caee779a707ff79b2ed9da667a
Fustor <zfaust88@gmail.com>
2018-07-16 13:28:45 -0500

    Fixed problems with not selecting date when adding schedule and now allows for deletion of scheduels

--- hotterSchedules/ViewEmployeeSchedule.xaml.cs
+++ hotterSchedules/ViewEmployeeSchedule.xaml.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 /*
  * TODO:
- * 1:Update after adding a new day
- * 2:Account for not selecting a day to add
- * 3:Only see columns for name, job, day, and hours
+ * DONE 1:Update after adding a new day
+ * DONE 2:Account for not selecting a day to add
+ * Deal with this 3:Only see columns for name, job, day, and hours
+ * 4. Colors for days working
+ * DONE 5. Ability to delete days
  * 
  */
 namespace hotterSchedules
@@ -31,17 +24,36 @@ namespace hotterSchedules
         {
             CurrentEmployee = CurEmployee;
             db = data;
-            var Schedules = (from e in db.Schedules where e.EmployeeId == CurrentEmployee.EmployeeID select e).ToList();
+            var Schedules = (from s in db.Schedules where s.EmployeeId == CurrentEmployee.EmployeeID select s).ToList();
             InitializeComponent();
             EmployeeNameLabel.Content = CurrentEmployee.name;
+           
             ScheduleGrid.ItemsSource = Schedules;
+            ScheduleCalendar.SelectedDate = DateTime.Today;
+            
+           // ScheduleGrid.Columns[1].Visibility = Visibility.Collapsed;
+            //ScheduleGrid.Columns[7].Visibility = Visibility.Collapsed;
         }
 
         private void AddToScheduleButton_Click(object sender, RoutedEventArgs e)
         {
-            Schedule NewDay = new Schedule(ScheduleCalendar.SelectedDate.Value,CurrentEmployee.EmployeeID,TimeInTextbox.Text,TimeOutTextbox.Text,JobTextbox.Text);
-            db.Schedules.Add(NewDay);
+           
+                Schedule NewDay = new Schedule(ScheduleCalendar.SelectedDate.Value, CurrentEmployee.EmployeeID, TimeInTextbox.Text, TimeOutTextbox.Text, JobTextbox.Text);
+                db.Schedules.Add(NewDay);
+                db.SaveChanges();
+                var Schedules = (from s in db.Schedules where s.EmployeeId == CurrentEmployee.EmployeeID select s).ToList();
+                ScheduleGrid.ItemsSource = Schedules;
+           
+        }
+
+        private void DeleteItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            Schedule SelectedSchedule = (Schedule)ScheduleGrid.SelectedItem;
+            db.Schedules.Attach(SelectedSchedule);
+            db.Schedules.Remove(SelectedSchedule);
             db.SaveChanges();
+            var Schedules = (from s in db.Schedules where s.EmployeeId == CurrentEmployee.EmployeeID select s).ToList();
+            ScheduleGrid.ItemsSource = Schedules;
             
         }
     }
